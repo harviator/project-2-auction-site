6fc786333c3564b13e5bc15ca9818b5f5456ee97
Elison Liu <elisonliu97@gmail.com>
2021-06-30 11:01:24 -0400

    fixed seed and create userRoutes

--- models/User.js
+++ models/User.js
@@ -1,10 +1,15 @@
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt')
 
 const sequelize = require('../config/connection.js');
 
-class User extends Model {}
+class User extends Model {
+  checkPassword(checkpw) {
+    return bcrypt.compareSync(checkpw, this.password)
+  }
+}
 
-User.init (
+User.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -29,6 +34,12 @@ User.init (
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      }
+    },
     sequelize,
     timestamps: false,
     freezeTableName: true,
