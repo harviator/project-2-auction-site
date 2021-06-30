15c72d40e0cfc2b0c2efc845471ac2f8bd156be0
Elison Liu <elisonliu97@gmail.com>
2021-06-30 13:43:54 -0400

    syntax errors

--- controllers/api/postingRoutes.js
+++ controllers/api/postingRoutes.js
@@ -21,7 +21,7 @@ router.delete('/:id', async (req, res) => {
     try {
         const postingData = await Posting.destroy({
             where: {
-                id = req.params.id,
+                id : req.params.id,
                 user_id: req.session.user_id,
             },
         });
