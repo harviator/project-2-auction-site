7fca97f22a0bb92cd59f742258a290db140524a2
Harviator <78506767+harviator@users.noreply.github.com>
2021-07-01 11:33:50 -0400

    made correction to login.js

--- public/js/login.js
+++ public/js/login.js
@@ -25,7 +25,7 @@ const signupFormHandler = async (event) => {
   const username = document.querySelector('#username-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
 
-  if (name && email && password) {
+  if (username && password) {
     const response = await fetch('/api/users', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
